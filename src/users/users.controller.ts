505081a528ca97a19b828a358c0616390e3ee10b
Juan L <33398520+juanlucasl@users.noreply.github.com>
2022-09-15 20:39:32 -0300

    Implement User methods

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,76 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UseInterceptors,
+} from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  /**
+   * Creates a new user with the given details.
+   *
+   * @param body User email and password
+   */
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    this.usersService.create(body.email, body.password);
+  }
+
+  /**
+   * Find a user given their id.
+   *
+   * @param id Id to look for.
+   * @returns User with matching id.
+   */
+  @UseInterceptors(ClassSerializerInterceptor)
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.usersService.findOne(parseInt(id));
+  }
+
+  /**
+   * Find all users associated to a given email address.
+   *
+   * @param email Email address to look for.
+   * @returns Array of matching users.
+   */
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
+
+  /**
+   * Update a user with a given id.
+   *
+   * @param id User id.
+   * @param body User attributes to update.
+   * @returns Updated user.
+   */
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
+  }
+
+  /**
+   * Remove a user with a given id.
+   *
+   * @param id Id of the user to remove.
+   * @returns Removed user.
+   */
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,72 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repository: Repository<User>) {}
+
+  /**
+   * Creates a new user with the given details.
+   *
+   * @param email User email.
+   * @param password User password.
+   */
+  create(email: string, password: string) {
+    const user = this.repository.create({ email, password });
+
+    return this.repository.save(user);
+  }
+
+  /**
+   * Find a user given their id.
+   *
+   * @param id Id to look for.
+   * @returns User with matching id.
+   * @throws NotFoundException
+   */
+  async findOne(id: number) {
+    const user = await this.repository.findOneBy({ id });
+    if (!user) throw new NotFoundException('User not found');
+    return user;
+  }
+
+  /**
+   * Find all users associated to a given email address.
+   *
+   * @param email Email address to look for.
+   * @returns Array of matching users.
+   */
+  find(email: string) {
+    return this.repository.findBy({ email });
+  }
+
+  /**
+   * Update a user with a given id.
+   *
+   * @param id User id.
+   * @param attrs User attributes to update.
+   * @returns Updated user.
+   * @throws NotFoundException
+   */
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) throw new NotFoundException('User not found');
+    Object.assign(user, attrs);
+    return this.repository.save(user);
+  }
+
+  /**
+   * Remove a user with a given id.
+   *
+   * @param id Id of the user to remove.
+   * @returns Removed user.
+   * @throws NotFoundException
+   */
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) throw new NotFoundException('User not found');
+    return this.repository.remove(user);
+  }
+}
