cb6a1f6204fc0cc4769689924cf52c56021d7c14
Juan L <33398520+juanlucasl@users.noreply.github.com>
2022-09-17 14:24:16 -0300

    Implement session cookie

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+const cookieSession = require('cookie-session');
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cookieSession({
+      keys: ['developmentdummykey'],
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
